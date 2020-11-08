86de3b2b96aa019763150c7afdab04fdb38abb4a
mark mahowald <mark.mahowald@gates.com>
2020-11-08 10:23:10 -0700

    MM 11/8/2020 Initial Commit

---
+++ ResusableWpfUI/Ui.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ui.Wpf")]
+[assembly: AssemblyDescription("A library of reusable styles themes, controls, icons, etc for a WPF application. ")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ui.Wpf")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly 
+)]
+
+//The tutorial that this project started from used a mocked up training website called Globomantics.com/2019/ui/wpf. I have used this as a place holder. 
+[assembly: XmlnsDefinition("http://NotARealSite.com/2019/ui/wpf", "MarkOMantics.Ui.Wpf")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
